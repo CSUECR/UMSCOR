4053e6358cfe6ec62b27eb23a74f696b3ad06c0d
hoho18 <qcl4563@163.com>
2014-07-20 18:31:23 +0800

    Index OK

--- MorSun/Scripts/Common/HOHO18.Common.js
+++ MorSun/Scripts/Common/HOHO18.Common.js
@@ -105,3 +105,24 @@ function getVerifyCode(text, imgControl) {
         $(imgControl).attr("src", imgSrc);
     }
 }
+
+
+/*****************************返回顶部方法******************************************************/
+function backtop() {
+    //当滚动条的位置处于距顶部100像素以下时，跳转链接出现，否则消失
+    $(function () {
+        $(window).scroll(function () {
+            if ($(window).scrollTop() > 200) {
+                $("#back-to-top").fadeIn(1500);
+            }
+            else {
+                $("#back-to-top").fadeOut(1500);
+            }
+        });
+        //当点击跳转链接后，回到页面顶部位置
+        $("#back-to-top").click(function () {
+            $('body,html').animate({ scrollTop: 0 }, 1000);
+            return false;
+        });
+    });
+}
