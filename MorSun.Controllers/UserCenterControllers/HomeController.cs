c64bc2fb2c7133045feab62f9f3a7d6fdcd6ea42
hoho18 <qcl4563@163.com>
2014-08-04 16:31:53 +0800

    Webconfig加密暂时版，如果拿到服务器权限还是会被解密

--- MorSun.Controllers/UserCenterControllers/HomeController.cs
+++ MorSun.Controllers/UserCenterControllers/HomeController.cs
@@ -6,6 +6,8 @@ using System.Web.Mvc;
 using MorSun.Bll;
 using MorSun.Model;
 using System.Web.Routing;
+using System.Web.Configuration;
+using System.Configuration;
 
 namespace MorSun.Controllers
 {
@@ -74,6 +76,70 @@ namespace MorSun.Controllers
                 FormsService.SignIn(user.aspnet_Users.UserName,false);
             }
             return RedirectToAction("Index", "Home");
+        }
+
+        /// <summary>
+        /// webconfig加密解密
+        /// </summary>
+        /// <returns></returns>
+        public string ENCWeb()
+        {
+            var provider = "RSAProtectedConfigurationProvider";
+            var section = "connectionStrings";
+            var section1 = "quartz";
+            var section2 = "log4net";
+            Configuration confg = WebConfigurationManager.OpenWebConfiguration(Request.ApplicationPath);
+            ConfigurationSection configSect = confg.GetSection(section);            
+            if (configSect != null)
+            {
+                configSect.SectionInformation.ProtectSection(provider);
+                confg.Save();
+            }
+
+            ConfigurationSection configSect1 = confg.GetSection(section1);
+            if (configSect1 != null)
+            {
+                configSect1.SectionInformation.ProtectSection(provider);
+                confg.Save();
+            }
+
+            ConfigurationSection configSect2 = confg.GetSection(section2);
+            if (configSect2 != null)
+            {
+                configSect2.SectionInformation.ProtectSection(provider);
+                confg.Save();
+            }
+            return "";
+        }
+
+        public string DECWeb()
+        {
+            var provider = "RSAProtectedConfigurationProvider";
+            var section = "connectionStrings";
+            var section1 = "quartz";
+            var section2 = "log4net";
+            Configuration config = WebConfigurationManager.OpenWebConfiguration(Request.ApplicationPath);
+            ConfigurationSection configSect = config.GetSection(section);
+            if (configSect.SectionInformation.IsProtected)
+            {
+                configSect.SectionInformation.UnprotectSection();
+                config.Save();
+            }
+
+            ConfigurationSection configSect1 = config.GetSection(section1);
+            if (configSect1.SectionInformation.IsProtected)
+            {
+                configSect1.SectionInformation.UnprotectSection();
+                config.Save();
+            }
+
+            ConfigurationSection configSect2 = config.GetSection(section2);
+            if (configSect2.SectionInformation.IsProtected)
+            {
+                configSect2.SectionInformation.UnprotectSection();
+                config.Save();
+            }
+            return "";
         }
     }
 }
