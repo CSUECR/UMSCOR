a100876aa2edb532ee70556b622930a5d89e603c
hoho18 <qcl4563@163.com>
2014-10-22 07:00:37 +0800

    取马币值的优化，现在已经可以实时计算

--- MorSun.Controllers/Base/BasisController.cs
+++ MorSun.Controllers/Base/BasisController.cs
@@ -133,6 +133,7 @@ namespace MorSun.Controllers
         /// <returns></returns>
         private static UserMaBi GetUserMaBiByUId(Guid userId)
         {
+            //取出当前用户已结算的马币值
             var umb = new BaseBll<bmUserMaBi>().All.Where(p => p.UserId == userId);
             //币种
             var mabi = Guid.Parse(Reference.马币类别_马币);
@@ -146,6 +147,23 @@ namespace MorSun.Controllers
             userMaBi.mabi = mabiO == null ? 0 : mabiO.MaBiNum.Value;
             userMaBi.bbi = bbiO == null ? 0 : bbiO.MaBiNum.Value;
             userMaBi.banbi = banbiO == null ? 0 : banbiO.MaBiNum.Value;
+
+            //减去未结算的马币值
+            var rbll = new BaseBll<bmUserMaBiRecord>();
+            var nonSettleMBR = rbll.All.Where(p => p.IsSettle == false && p.UserId == userId);
+            
+            //加马币
+            var mabisum = nonSettleMBR.Where(p => p.MaBiRef == mabi).Sum(p => p.MaBiNum);
+            //加邦币
+            var bbisum = nonSettleMBR.Where(p => p.MaBiRef == bbi).Sum(p => p.MaBiNum);
+            //加绑币
+            var banbisum = nonSettleMBR.Where(p => p.MaBiRef == banbi).Sum(p => p.MaBiNum);
+            if (mabisum != null && mabisum > 0)
+                userMaBi.mabi += mabisum == null ? 0 : mabisum.Value;
+            if (bbisum != null && bbisum > 0)
+                userMaBi.bbi += bbisum == null ? 0 : bbisum.Value;
+            if (banbisum != null && banbisum > 0)
+                userMaBi.banbi += banbisum == null ? 0 : banbisum.Value;
 
             return userMaBi;
         }
