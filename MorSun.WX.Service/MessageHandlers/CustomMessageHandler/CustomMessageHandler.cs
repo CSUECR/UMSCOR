2759d06a0347cc794cacffe6c06e3d73dba832c8
hoho18 <qcl4563@163.com>
2014-10-29 16:48:19 +0800

    微信发送多条消息的处理未完成

--- MorSun.WX.Service/MessageHandlers/CustomMessageHandler/CustomMessageHandler.cs
+++ MorSun.WX.Service/MessageHandlers/CustomMessageHandler/CustomMessageHandler.cs
@@ -2,6 +2,7 @@
 using System.Configuration;
 using System.IO;
 using System.Text;
+using System.Linq;
 using Senparc.Weixin.MP.Agent;
 using Senparc.Weixin.Context;
 using Senparc.Weixin.MP.Entities;
@@ -212,63 +213,66 @@ namespace MorSun.WX.ZYB.Service.CustomMessageHandler
             var model = SubmitQuestion(requestMessage);
 
             var responseMessage = CreateResponseMessage<ResponseMessageNews>();
-            responseMessage.Articles.Add(new Article()
-            {
-                Title = ("问题编号：" + model.AutoGrenteId + " ") + ((model.MaBiNum == 0 || model.MaBiNum == null) ? "0消耗" : ("消耗" + (model.MaBiNum == null ? "0" : model.MaBiNum.ToString() + GetReferenceValue(model.MaBiRef)))),
-                Description = ((model.MaBiNum == 0 || model.MaBiNum == null) ? "免费提问" : ("消耗" + (model.MaBiNum == null ? "0" : model.MaBiNum.ToString() + GetReferenceValue(model.MaBiRef)))) + (" 问题编号：" + model.AutoGrenteId),
-                PicUrl = requestMessage.PicUrl,
-                Url = requestMessage.PicUrl
-            });
-            responseMessage.Articles.Add(new Article()
-            {//眼睛图片
-                Title = "看答案",
-                Description = "看答案",
-                PicUrl = "",
-                Url = CFG.网站域名 + "/QA/Q/" + model.ID.ToString()
-            });//再增加 加码 求解题思路            
-            responseMessage.Articles.Add(new Article()
-            {//美元图片
-                Title = "加马币",
-                Description = "加马币",
-                PicUrl = "",
-                Url = CFG.网站域名 + "/QA/Q/" + model.ID.ToString()
-            });
-            responseMessage.Articles.Add(new Article()
-            {//问号图片
-                Title = "求思路",
-                Description = "求思路",
-                PicUrl = "",
-                Url = CFG.网站域名 + "/QA/Q/" + model.ID.ToString()
-            });
-            responseMessage.Articles.Add(new Article()
-            {//问号图片
-                Title = "直接看答案请发送:   " + CFG.快速看答案 + " " + model.AutoGrenteId,
-                Description = "直接看答案",
-                PicUrl = "",
-                Url = CFG.网站域名 + "/QA/Q/" + model.ID.ToString()
-            });           
-            
-            //判断用户是否绑定，未绑定显示注册账号并绑定，已经绑定显示分享链接
-            var userWeiXin = new UserMaBiService().GetUserByWeiXinId(requestMessage.FromUserName);
-            if(userWeiXin == null)
+            if (model != null)
             {
                 responseMessage.Articles.Add(new Article()
-                {//问号图片
-                    Title = "注册账号并绑定",
-                    Description = "注册账号并绑定",
-                    PicUrl = "",
-                    Url = CFG.网站域名 + "/Account/Register"
+                {
+                    Title = ("问题编号：" + model.AutoGrenteId + " ") + ((model.MaBiNum == 0 || model.MaBiNum == null) ? "0消耗" : ("消耗" + (model.MaBiNum == null ? "0" : model.MaBiNum.ToString() + GetReferenceValue(model.MaBiRef)))),
+                    Description = ((model.MaBiNum == 0 || model.MaBiNum == null) ? "免费提问" : ("消耗" + (model.MaBiNum == null ? "0" : model.MaBiNum.ToString() + GetReferenceValue(model.MaBiRef)))) + (" 问题编号：" + model.AutoGrenteId),
+                    PicUrl = requestMessage.PicUrl,
+                    Url = requestMessage.PicUrl
                 });
-            }
-            else
-            {
+                responseMessage.Articles.Add(new Article()
+                {//眼睛图片
+                    Title = "看答案",
+                    Description = "看答案",
+                    PicUrl = "",
+                    Url = CFG.网站域名 + "/QA/Q/" + model.ID.ToString()
+                });//再增加 加码 求解题思路            
+                responseMessage.Articles.Add(new Article()
+                {//美元图片
+                    Title = "加马币",
+                    Description = "加马币",
+                    PicUrl = "",
+                    Url = CFG.网站域名 + "/QA/Q/" + model.ID.ToString()
+                });
                 responseMessage.Articles.Add(new Article()
                 {//问号图片
-                    Title = "分享给朋友",
-                    Description = "分享给朋友",
+                    Title = "求思路",
+                    Description = "求思路",
                     PicUrl = "",
-                    Url = CFG.网站域名 + "/Account/Register/" + SecurityHelper.Encrypt(requestMessage.FromUserName)
+                    Url = CFG.网站域名 + "/QA/Q/" + model.ID.ToString()
                 });
+                responseMessage.Articles.Add(new Article()
+                {//问号图片
+                    Title = "直接看答案请发送:   " + CFG.快速看答案 + " " + model.AutoGrenteId,
+                    Description = "直接看答案",
+                    PicUrl = "",
+                    Url = CFG.网站域名 + "/QA/Q/" + model.ID.ToString()
+                });
+
+                //判断用户是否绑定，未绑定显示注册账号并绑定，已经绑定显示分享链接
+                var userWeiXin = new UserMaBiService().GetUserByWeiXinId(requestMessage.FromUserName);
+                if (userWeiXin == null)
+                {
+                    responseMessage.Articles.Add(new Article()
+                    {//问号图片
+                        Title = "注册账号并绑定",
+                        Description = "注册账号并绑定",
+                        PicUrl = "",
+                        Url = CFG.网站域名 + "/Account/Register"
+                    });
+                }
+                else
+                {
+                    responseMessage.Articles.Add(new Article()
+                    {//问号图片
+                        Title = "分享给朋友",
+                        Description = "分享给朋友",
+                        PicUrl = "",
+                        Url = CFG.网站域名 + "/Account/Register/" + SecurityHelper.Encrypt(requestMessage.FromUserName)
+                    });
+                }
             }
             return responseMessage;
         }
@@ -283,95 +287,99 @@ namespace MorSun.WX.ZYB.Service.CustomMessageHandler
             var bll = new BaseBll<bmQA>();           
 
             var model = new bmQA();
+            if (bll.All.Where(p => p.MsgId == model.MsgId).FirstOrDefault() == null)
+            {//已经存在的问题，不再保存进系统
+                model.ID = Guid.NewGuid();
+                model.WeiXinId = requestMessage.FromUserName;
+                model.QARef = Guid.Parse(Reference.问答类别_问题);
+                model.MsgId = requestMessage.MsgId == null ? "" : requestMessage.MsgId.ToString();
+                model.MsgType = Guid.Parse(Reference.微信消息类别_图片);
+                model.MediaId = requestMessage.MediaId;
+                model.PicUrl = requestMessage.PicUrl;
 
-            model.ID = Guid.NewGuid();
-            model.WeiXinId = requestMessage.FromUserName;
-            model.QARef = Guid.Parse(Reference.问答类别_问题);   
-            model.MsgId = requestMessage.MsgId == null ? "" : requestMessage.MsgId.ToString();
-            model.MsgType = Guid.Parse(Reference.微信消息类别_图片);
-            model.MediaId = requestMessage.MediaId;
-            model.PicUrl = requestMessage.PicUrl;
+                model.RegTime = DateTime.Now;
+                model.ModTime = DateTime.Now;
+                model.FlagTrashed = false;
+                model.FlagDeleted = false;
 
-            model.RegTime = DateTime.Now;
-            model.ModTime = DateTime.Now;
-            model.FlagTrashed = false;
-            model.FlagDeleted = false;
-
-            //问题消耗马币和分配答题用户处理
-            var userMaBi = new UserMaBiService().GetUserCurrentMaBi(requestMessage.FromUserName);
-            //消耗马币
-            if(userMaBi != null)
-            {
-                var defMaBi = Convert.ToDecimal(CFG.提问默认收费马币值);
-                if(userMaBi.UMB.BBi >= defMaBi || userMaBi.UMB.MaBi >= defMaBi)
+                //问题消耗马币和分配答题用户处理
+                var userMaBi = new UserMaBiService().GetUserCurrentMaBi(requestMessage.FromUserName);
+                //消耗马币
+                if (userMaBi != null)
                 {
-                    if(userMaBi.UMB.BBi >= defMaBi)
+                    var defMaBi = Convert.ToDecimal(CFG.提问默认收费马币值);
+                    if (userMaBi.UMB.BBi >= defMaBi || userMaBi.UMB.MaBi >= defMaBi)
                     {
-                        //消耗邦币处理
-                        model.MaBiRef = Guid.Parse(Reference.马币类别_邦币);
-                        model.MaBiNum = defMaBi;
+                        if (userMaBi.UMB.BBi >= defMaBi)
+                        {
+                            //消耗邦币处理
+                            model.MaBiRef = Guid.Parse(Reference.马币类别_邦币);
+                            model.MaBiNum = defMaBi;
 
-                        //添加马币消费记录
-                        var addMBR = new AddMBRModel();
-                        addMBR.UIds.Add(userMaBi.UserId);
+                            //添加马币消费记录
+                            var addMBR = new AddMBRModel();
+                            addMBR.UIds.Add(userMaBi.UserId);
 
-                        addMBR.QAId = model.ID;
-                        addMBR.SR = Guid.Parse(Reference.马币来源_扣取);
-                        addMBR.MBR = Guid.Parse(Reference.马币类别_邦币);
-                        addMBR.MBN = 0 - defMaBi;
-                        new UserMaBiService().AddUMBRByQA(addMBR, false);
+                            addMBR.QAId = model.ID;
+                            addMBR.SR = Guid.Parse(Reference.马币来源_扣取);
+                            addMBR.MBR = Guid.Parse(Reference.马币类别_邦币);
+                            addMBR.MBN = 0 - defMaBi;
+                            new UserMaBiService().AddUMBRByQA(addMBR, false);
+                        }
+                        else if (userMaBi.UMB.MaBi >= defMaBi)
+                        {
+                            //消耗马币处理
+                            model.MaBiRef = Guid.Parse(Reference.马币类别_马币);
+                            model.MaBiNum = defMaBi;
+
+                            //添加马币消费记录
+                            var addMBR = new AddMBRModel();
+                            addMBR.UIds.Add(userMaBi.UserId);
+
+                            addMBR.QAId = model.ID;
+                            addMBR.SR = Guid.Parse(Reference.马币来源_扣取);
+                            addMBR.MBR = Guid.Parse(Reference.马币类别_马币);
+                            addMBR.MBN = 0 - defMaBi;
+                            new UserMaBiService().AddUMBRByQA(addMBR, false);
+                        }
                     }
-                    else if(userMaBi.UMB.MaBi >= defMaBi)
+                    else
                     {
-                        //消耗马币处理
-                        model.MaBiRef = Guid.Parse(Reference.马币类别_马币);
-                        model.MaBiNum = defMaBi;
-
-                        //添加马币消费记录
-                        var addMBR = new AddMBRModel();
-                        addMBR.UIds.Add(userMaBi.UserId);
-
-                        addMBR.QAId = model.ID;
-                        addMBR.SR = Guid.Parse(Reference.马币来源_扣取);
-                        addMBR.MBR = Guid.Parse(Reference.马币类别_马币);
-                        addMBR.MBN = 0 - defMaBi;
-                        new UserMaBiService().AddUMBRByQA(addMBR, false);
+                        //马币与邦币不足时的处理  都不足时不作任何处理
                     }
                 }
                 else
                 {
-                    //马币与邦币不足时的处理  都不足时不作任何处理
+                    //未绑定的微信号  也不作任何处理
                 }
-            }
-            else
-            {
-                //未绑定的微信号  也不作任何处理
-            }
 
-            //问题分配处理
-            var qadbll = new BaseBll<bmQADistribution>();
-            var qaModel = new bmQADistribution();
+                //问题分配处理
+                var qadbll = new BaseBll<bmQADistribution>();
+                var qaModel = new bmQADistribution();
 
-            qaModel.ID = Guid.NewGuid();
-            qaModel.QAId = model.ID;
-            qaModel.DistributionTime = DateTime.Now;
-            qaModel.RegTime = DateTime.Now;
-            if(model.MaBiRef == null || model.MaBiNum == null || model.MaBiNum == 0)
-            {
-                //免费问题的分配
-                var bmOU = new UserQADistributionService().GetQADistribution(false);
-                qaModel.WeiXinId = bmOU == null ? CFG.默认免费问题微信号 : bmOU.WeiXinId;
+                qaModel.ID = Guid.NewGuid();
+                qaModel.QAId = model.ID;
+                qaModel.DistributionTime = DateTime.Now;
+                qaModel.RegTime = DateTime.Now;
+                if (model.MaBiRef == null || model.MaBiNum == null || model.MaBiNum == 0)
+                {
+                    //免费问题的分配
+                    var bmOU = new UserQADistributionService().GetQADistribution(false);
+                    qaModel.WeiXinId = bmOU == null ? CFG.默认免费问题微信号 : bmOU.WeiXinId;
+                }
+                else
+                {
+                    //收费问题的分配
+                    var bmOU = new UserQADistributionService().GetQADistribution(true);
+                    qaModel.WeiXinId = bmOU == null ? CFG.默认收费问题微信号 : bmOU.WeiXinId;
+                }
+                qadbll.Insert(qaModel, false);
+                //判断是否有该条记录，如果没有，则添加 微信会重复改善数据
+                if (bll.All.Where(p => p.MsgId == model.MsgId).FirstOrDefault() == null)
+                    bll.Insert(model);
             }
-            else
-            {
-                //收费问题的分配
-                var bmOU = new UserQADistributionService().GetQADistribution(true);
-                qaModel.WeiXinId = bmOU == null ? CFG.默认收费问题微信号 : bmOU.WeiXinId;
-            }
-            qadbll.Insert(qaModel, false);
-            bll.Insert(model);
             //为了取自增长ID
-            model = bll.GetModel(model.ID);
+            model = bll.All.Where(p => p.MsgId == model.MsgId).FirstOrDefault();
             return model;
         }
 
