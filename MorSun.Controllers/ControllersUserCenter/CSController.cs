5216d122fb401b295e254fd5ce8b63faecc5451d
hoho18 <qcl4563@163.com>
2014-11-17 22:33:47 +0800

    功能页面补�

---
+++ MorSun.Controllers/ControllersUserCenter/CSController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MorSun.Controllers.ViewModel;
+using MorSun.Model;
+
+namespace MorSun.Controllers
+{    
+    [HandleError]
+    public class CSController : Controller
+    {
+        public ActionResult C()
+        {
+            var indexModel = new IndexModel();
+            return View(indexModel);
+        }
+        
+    }
+}
