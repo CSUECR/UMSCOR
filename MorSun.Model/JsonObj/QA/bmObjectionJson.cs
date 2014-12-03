3f72c38fd220b4829ee84288c59e305e02861e75
hoho18 <qcl4563@163.com>
2014-12-03 14:34:14 +0800

    增加QA与用户微信绑定JSON类

---
+++ MorSun.Model/JsonObj/QA/bmObjectionJson.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Linq;
+using System.Web.Mvc;
+using HOHO18.Common;
+
+namespace MorSun.Model
+{
+    public class bmObjectionJsonJson
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public Guid ID
+        { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public Guid QAId
+        { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public Guid UserId
+        { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public Guid WeiXinId
+        { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime? SubmitTime
+        { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ObjectionExplain
+        { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public Guid HandleUser
+        { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public Guid Result
+        { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime? HandleTime
+        { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string HandleExplain
+        { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int? Sort
+        { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public Guid RegUser
+        { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime? RegTime
+        { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime? ModTime
+        { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool FlagTrashed
+        { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool FlagDeleted
+        { get; set; }
+    }
+
+}
---
+++ MorSun.Model/JsonObj/QA/bmQAJson.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Linq;
+using System.Web.Mvc;
+using HOHO18.Common;
+
+namespace MorSun.Model
+{    
+    public class bmQAJson
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public Guid ID
+        { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public long AutoGrenteId
+        { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public Guid ParentId
+        { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public Guid UserId
+        { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string WeiXinId
+        { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public Guid QARef
+        { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string QAContent
+        { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string MsgId
+        { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public Guid MsgType
+        { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string MediaId
+        { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string PicUrl
+        { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int? Sort
+        { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public Guid RegUser
+        { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime? RegTime
+        { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime? ModTime
+        { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool FlagTrashed
+        { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool FlagDeleted
+        { get; set; }
+    }
+
+}
