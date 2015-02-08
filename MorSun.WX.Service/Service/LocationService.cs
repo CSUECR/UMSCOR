0712e7259d5b6028952637aa206397f310a416c1
hoho18 <qcl4563@163.com>
2015-02-09 00:24:30 +0800

    位置再测试

--- MorSun.WX.Service/Service/LocationService.cs
+++ MorSun.WX.Service/Service/LocationService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using Senparc.Weixin.MP.Entities;
 using Senparc.Weixin.MP.Entities.GoogleMap;
+using Senparc.Weixin.MP.Entities.BaiduMap;
 using Senparc.Weixin.MP.Helpers;
 
 namespace MorSun.WX.ZYB.Service
@@ -23,14 +24,34 @@ namespace MorSun.WX.ZYB.Service
             var mapSize = "480x600";
             var mapUrl = GoogleMapHelper.GetGoogleStaticMap(19 /*requestMessage.Scale*//*微信和GoogleMap的Scale不一致，这里建议使用固定值*/,
                                                             markersList, mapSize);
+
+            var bdmarkersList = new List<BaiduMarkers>();
+            bdmarkersList.Add(new BaiduMarkers()
+            {
+                Latitude = requestMessage.Location_X,
+                Longitude = requestMessage.Location_Y,
+                Color = "red",
+                Label = "S",
+                url = "www.bungma.com",
+            });
+            var bdMapUrl = BaiduMapHelper.GetBaiduStaticMap(requestMessage.Location_X, requestMessage.Location_Y, requestMessage.Scale, 1, bdmarkersList, 400, 300);
+            //responseMessage.Articles.Add(new Article()
+            //{
+            //    Description = string.Format("您刚才发送了地理位置信息。Location_X：{0}，Location_Y：{1}，Scale：{2}，标签：{3}",
+            //                  requestMessage.Location_X, requestMessage.Location_Y,
+            //                  requestMessage.Scale, requestMessage.Label),
+            //    PicUrl = mapUrl,
+            //    Title = "定位地点周边地图",
+            //    Url = mapUrl
+            //});
             responseMessage.Articles.Add(new Article()
             {
                 Description = string.Format("您刚才发送了地理位置信息。Location_X：{0}，Location_Y：{1}，Scale：{2}，标签：{3}",
                               requestMessage.Location_X, requestMessage.Location_Y,
                               requestMessage.Scale, requestMessage.Label),
-                PicUrl = mapUrl,
+                PicUrl = bdMapUrl,
                 Title = "定位地点周边地图",
-                Url = mapUrl
+                Url = bdMapUrl
             });
             //responseMessage.Articles.Add(new Article()
             //{
