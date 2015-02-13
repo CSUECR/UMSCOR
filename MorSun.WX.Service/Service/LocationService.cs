24c5de2333ab984f13d398e83d6f8d4da6f19fa4
hoho18 <qcl4563@163.com>
2015-02-13 09:56:54 +0800

    细节调整

--- MorSun.WX.Service/Service/LocationService.cs
+++ MorSun.WX.Service/Service/LocationService.cs
@@ -25,34 +25,36 @@ namespace MorSun.WX.ZYB.Service
             var mapUrl = GoogleMapHelper.GetGoogleStaticMap(19 /*requestMessage.Scale*//*微信和GoogleMap的Scale不一致，这里建议使用固定值*/,
                                                             markersList, mapSize);
 
-            var bdmarkersList = new List<BaiduMarkers>();
-            bdmarkersList.Add(new BaiduMarkers()
-            {
-                Latitude = requestMessage.Location_X,
-                Longitude = requestMessage.Location_Y,
-                Color = "red",
-                Label = "S",
-                url = "www.bungma.com",
-            });
-            var bdMapUrl = BaiduMapHelper.GetBaiduStaticMap(requestMessage.Location_X, requestMessage.Location_Y, requestMessage.Scale, 1, bdmarkersList, 400, 300);
-            //responseMessage.Articles.Add(new Article()
-            //{
-            //    Description = string.Format("您刚才发送了地理位置信息。Location_X：{0}，Location_Y：{1}，Scale：{2}，标签：{3}",
-            //                  requestMessage.Location_X, requestMessage.Location_Y,
-            //                  requestMessage.Scale, requestMessage.Label),
-            //    PicUrl = mapUrl,
-            //    Title = "定位地点周边地图",
-            //    Url = mapUrl
-            //});
             responseMessage.Articles.Add(new Article()
             {
                 Description = string.Format("您刚才发送了地理位置信息。Location_X：{0}，Location_Y：{1}，Scale：{2}，标签：{3}",
                               requestMessage.Location_X, requestMessage.Location_Y,
                               requestMessage.Scale, requestMessage.Label),
-                PicUrl = bdMapUrl,
-                Title = "定位地点周边地图",
-                Url = bdMapUrl
+                PicUrl = mapUrl,
+                Title = requestMessage.Label,
+                Url = mapUrl
             });
+
+            //var bdmarkersList = new List<BaiduMarkers>();
+            //bdmarkersList.Add(new BaiduMarkers()
+            //{
+            //    Latitude = requestMessage.Location_X,
+            //    Longitude = requestMessage.Location_Y,
+            //    Color = "red",
+            //    Label = "S",
+            //    url = "www.bungma.com",
+            //});
+            //var bdMapUrl = BaiduMapHelper.GetBaiduStaticMap(requestMessage.Location_X, requestMessage.Location_Y, requestMessage.Scale, 1, bdmarkersList, 400, 300);
+            
+            //responseMessage.Articles.Add(new Article()
+            //{
+            //    Description = string.Format("您刚才发送了地理位置信息。Location_X：{0}，Location_Y：{1}，Scale：{2}，标签：{3}",
+            //                  requestMessage.Location_X, requestMessage.Location_Y,
+            //                  requestMessage.Scale, requestMessage.Label),
+            //    PicUrl = bdMapUrl,
+            //    Title = "定位地点周边地图",
+            //    Url = bdMapUrl
+            //});
             //responseMessage.Articles.Add(new Article()
             //{
             //    Title = "邦马网",
